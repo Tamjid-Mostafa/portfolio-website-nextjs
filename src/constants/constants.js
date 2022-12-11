a572023a569cdec960fa5d00850660706dfda58a
Tamjid-Mostafa <tamjid430@gmail.com>
2022-12-11 22:28:29 +0600

    changes in constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,6 +4,13 @@ export const projects = [
     description:
       "Using React, Node.js, Express & MongoDB, I build a Full Stack MERN Application - from start to finish. The App is called Your Moto and it is a simple used bike buy & sell app that allows users to post products with information also can boost their product advertisement. Buyers can make purchase their desire bike with their meeting locations and phone numbers also can book their product. I have used stripe to make purchase.",
     image: "https://i.ibb.co/tB6RpHj/1.jpg",
+    detailImage: {
+      page1: "",
+      page2: "",
+      page3: "",
+      page4: "",
+
+    },
     tags: ["MongoDB", "Express", "React", "Node", "Stripe"],
     source: "https://github.com/Tamjid-Mostafa/Your-Moto",
     visit: "https://yourmoto-45ca1.web.app/",
@@ -14,16 +21,30 @@ export const projects = [
     description:
       "In this website I have displayed some web development technology courses. Using React, Node.js, Express & MongoDB, I build a Full Stack MERN Application - from start to finish. The App is called Edu Vibe and it is a simple E-Learning platform that allows users to learn and purchase new courses.",
     image: "https://i.ibb.co/t2qXCWG/2.jpg",
+    detailImage: {
+      page1: "",
+      page2: "",
+      page3: "",
+      page4: "",
+
+    },
     tags: ["MongoDB", "Express", "React", "Node", "Firebase"],
     source: "https://github.com/Tamjid-Mostafa/Edu-Vibe",
     visit: "https://edu-vibe.web.app/",
     id: 1,
   },
   {
-    title: "WebRTC App",
+    title: "Photography Lux",
     description:
       "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
     image: "https://i.ibb.co/rMNXxdr/3.jpg",
+    detailImage: {
+      page1: "",
+      page2: "",
+      page3: "",
+      page4: "",
+
+    },
     tags: ["React", "Firebase", "Node", "Talwind CSS"],
     source: "https://github.com/Tamjid-Mostafa/PhotographyLux",
     visit: "https://photographylux-22fb4.web.app/",
