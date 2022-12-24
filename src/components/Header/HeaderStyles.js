4521b4f3643dbfaddb4c53ebe664fc20218b00f8
Tamjid-Mostafa <tamjid430@gmail.com>
2022-12-24 23:43:44 +0600

    Header grid fix

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -85,7 +85,7 @@ export const Div1 = styled.div`
   }
 `;
 export const Div2 = styled.div`
-  grid-area: 1 / 3 / 2 /4;
+  grid-area: 1 / 2 / 2 /4;
   display: flex;
   justify-content: space-around;
   transition: 0.3s ease-in;
